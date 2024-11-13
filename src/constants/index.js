6afdb843b868b4784f0f1ef0726c894035830ddf
Cduy11 <cduy527@gmail.com>
2024-11-13 11:25:04 +0700

    update apis, constants

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+// Token CyberSoft
+export const TOKEN_CYBERSOFT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3MiIsIkhldEhhblN0cmluZyI6IjI2LzA0LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0NTYyNTYwMDAwMCIsIm5iZiI6MTcxODAzODgwMCwiZXhwIjoxNzQ1NzczMjAwfQ.dB37yIT7JCR8-a4XuqTFyZfc5Mr5r0V5icCjUZrxFnI";
+
+// API URL
+export const API_URL = "https://movienew.cybersoft.edu.vn/api"
