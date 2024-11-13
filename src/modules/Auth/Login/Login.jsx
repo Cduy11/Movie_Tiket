c98b09fe3bfc5a1cae92b47d878863202e005908
Cduy11 <cduy527@gmail.com>
2024-11-13 14:48:28 +0700

    layout login, register

--- src/modules/Auth/Login/Login.jsx
+++ src/modules/Auth/Login/Login.jsx
@@ -1,9 +1,42 @@
-import { Box } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import "./Login.css";
+import { Button, Checkbox, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <Box>
-        Login
-    </Box>
-  )
+    <div className="login-container background-image">
+      <div className="login-box">
+        <div className="login-header">
+          <AccountCircle className="login-icon" />
+          <h2 className="login-title">Đăng nhập</h2>
+        </div>
+        <form>
+        <TextField
+          id="outlined-basic"
+          label="Tài Khoản"
+          variant="outlined"
+          className="login-input"
+          style={{ marginBottom: "20px" }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Mật Khẩu"
+          variant="outlined"
+          className="login-input"
+        />
+        </form>
+        <div className="remember-me">
+          <Checkbox />
+          <label htmlFor="remember" className="remember-me-label" >
+            Nhớ tài khoản
+          </label>
+        </div>
+        <Button className="login-button">ĐĂNG NHẬP</Button>
+        <p className="register-link">
+          <Link to="/auth/register">Bạn chưa có tài khoản? Đăng ký</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/modules/Auth/Register/Register.jsx
+++ src/modules/Auth/Register/Register.jsx
@@ -1,8 +1,87 @@
-
+import { TextField, Button, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from "react";
+import HttpsIcon from "@mui/icons-material/Https";
+import "./Register.css";
+import { Link } from "react-router-dom";
 export default function Register() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-        Register
+    <div className="register-container background-image">
+      <div className="register-box">
+        <div className="register-header">
+          <HttpsIcon className="login-icon" />
+          <h2 className="login-title">Đăng ký</h2>
+        </div>
+        <form>
+          <TextField
+            label="Tài Khoản"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Mật Khẩu"
+            variant="outlined"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            margin="normal"
+            InputProps={{
+              endAdornment: (
+                <IconButton onClick={handleClickShowPassword}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              ),
+            }}
+          />
+          <TextField
+            label="Nhập lại mật khẩu"
+            variant="outlined"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            margin="normal"
+            InputProps={{
+              endAdornment: (
+                <IconButton onClick={handleClickShowPassword}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              ),
+            }}
+          />
+          <TextField
+            label="Họ Tên"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            className="register-button"
+          >
+            ĐĂNG KÝ
+          </Button>
+        </form>
+        <p className="register-link">
+          <Link to="/auth/login">
+            {" "}
+            <span className="text-blue-500"> Bạn đã có tài khoản? </span>Đăng
+            nhập
+          </Link>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
